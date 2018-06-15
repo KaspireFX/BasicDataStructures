12875530593d14664172c0c0f46bea0a32daf9e9
Kaspire <chandlerje@vcu.edu>
2018-06-15 19:11:25 -0400

    Added SortedLinkedDictionary data structure

---
+++ DataStructures/Dictionaries/SortedLinkedDictionary.cs
@@ -0,0 +1,9 @@
+/*
+Author: Jacob Chandler
+File: SortedLinkedDictionary.cs
+Version 1.0.1
+Description: This file is the SortedLinkedDictionary class file which implements IDictionaryInterface.
+Date of Comment: 06:11:2018
+ */
+
+using System;
