2792736e48ff6d88ade63a84acf693ca660d1c27
Kaspire <chandlerje@vcu.edu>
2018-06-14 23:14:10 -0400

    Used for Iterator testing in Dictionaries

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -12,6 +12,7 @@ using DataStructures.Stacks;
 using DataStructures.Lists;
 using DataStructures.Queues;
 using DataStructures.Dictionaries;
+using DataStructures.Iterator;
 
 namespace TestConsole
 {
@@ -20,15 +21,29 @@ namespace TestConsole
         private static Stopwatch sw = new Stopwatch();
         public static void Main(string[] args)
         {
-            SortedArrayList<int> List = new SortedArrayList<int>();
+            LinkedDictionary<int, string> LDictionary = new LinkedDictionary<int, string>();
+            ArrayDictionary<int, string> ArrayDictionary = new ArrayDictionary<int, string>();
 
-            List.Add(6);
-            List.Add(2);
-            List.Add(5);
-            List.Add(1);
-            int[] Array = List.ToArray();
-            foreach(int i in Array) {
-                Console.WriteLine(i);
+            LDictionary.Add(8, "Phil");
+            LDictionary.Add(7, "bob");
+            LDictionary.Add(3, "mango");
+            LDictionary.Add(5, "bobby");
+
+            ArrayDictionary.Add(8, "bnana");
+            ArrayDictionary.Add(7, "f");
+            ArrayDictionary.Add(3, "d");
+            ArrayDictionary.Add(5, "b");
+
+            IIteratorInterface<string> Iterator1 = LDictionary.ValueIterator;
+            IIteratorInterface<string> Iterator2 = ArrayDictionary.ValueIterator;
+
+            while(Iterator1.HasNext()) {
+                Console.WriteLine(Iterator1.Next());
+            }
+
+            
+            while(Iterator2.HasNext()) {
+                Console.WriteLine(Iterator2.Next());
             }
         }
     }
