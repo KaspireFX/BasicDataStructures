bc3295643a7672d5798ae74059276033dfb811a8
Kaspire <chandlerje@vcu.edu>
2018-06-16 22:26:48 -0400

    Added Dictionary Iterator tests (Unsorted)

---
+++ DataStructures.Tests/DictionaryTests/ArrayDictionaryIteratorTests.cs
@@ -0,0 +1,49 @@
+/*
+Author: Jacob Chandler
+File: ArrayDictionaryIteratorTests.cs
+Version: 1.0.1
+Description: This file contains NUnit test functionality for the ArrayDictionaryIterator Data Structure class.
+Date of Comment: 06:16:2018
+ */
+
+using System;
+using NUnit.Framework;
+using DataStructures.Dictionaries;
+using DataStructures.Iterator;
+
+namespace Tests.DictionaryIteratorTests
+{
+    public class ArrayDictionaryIteratorTests
+    {
+
+        ArrayDictionary<int, string> ADictionary = new ArrayDictionary<int, string>();
+        IIteratorInterface<string> Iterator;
+
+        [SetUp]
+        public void Setup()
+        {
+            ADictionary.Add(6, "Bill");
+            ADictionary.Add(7, "Phil");
+            ADictionary.Add(12, "Frank");
+            ADictionary.Add(76, "Bobby");
+            Iterator = ADictionary.ValueIterator;
+        }
+
+        [Test]
+        public void TestNext() {
+            Assert.AreEqual("Bill", Iterator.Next());
+        }
+        
+        [Test]
+        public void TestHasNext() {
+            Assert.True(Iterator.HasNext());
+        }
+
+        [Test]
+        public void TestReset() {
+            Iterator.Next();
+            Iterator.Reset();
+            Assert.AreEqual("Bill", Iterator.Next());
+        }
+    }
+}
---
+++ DataStructures.Tests/DictionaryTests/LinkedDictionaryIteratorTests.cs
@@ -0,0 +1,49 @@
+/*
+Author: Jacob Chandler
+File: LinkedDictionaryIteratorTests.cs
+Version: 1.0.1
+Description: This file contains NUnit test functionality for the LinkedDictionaryIterator Data Structure class.
+Date of Comment: 06:16:2018
+ */
+
+using System;
+using NUnit.Framework;
+using DataStructures.Dictionaries;
+using DataStructures.Iterator;
+
+namespace Tests.DictionaryIteratorTests
+{
+    public class LinkedDictionaryTests
+    {
+
+        LinkedDictionary<int, string> LDictionary = new LinkedDictionary<int, string>();
+        IIteratorInterface<string> Iterator;
+
+        [SetUp]
+        public void Setup()
+        {
+            LDictionary.Add(5, "Bill");
+            LDictionary.Add(6, "Phil");
+            LDictionary.Add(7, "Frank");
+            LDictionary.Add(9, "Bobby");
+            Iterator = LDictionary.ValueIterator;
+        }
+
+        [Test]
+        public void TestNext() {
+            Assert.AreEqual("Bill", Iterator.Next());
+        }
+        
+        [Test]
+        public void TestHasNext() {
+            Assert.True(Iterator.HasNext());
+        }
+
+        [Test]
+        public void TestReset() {
+            Iterator.Next();
+            Iterator.Reset();
+            Assert.AreEqual("Bill", Iterator.Next());
+        }
+    }
+}
